d8318894c65ae14012655c4fc6e9af0ecbf8fff5
Franzi <franzi82@gmx.de>
2020-06-10 14:13:20 +0200

    Replace b in migratios closes #9

--- framework/swos/migrations/0001_initial.py
+++ framework/swos/migrations/0001_initial.py
@@ -26,7 +26,7 @@ class Migration(migrations.Migration):
                 ('description_usage', models.TextField(blank=True)),
                 ('description_creation', models.TextField(blank=True)),
                 ('order', models.PositiveIntegerField(default=0)),
-                ('type', models.CharField(blank=True, choices=[(b'change', b'change'), (b'state', b'state')], help_text=b'Type of indicator', max_length=20)),
+                ('type', models.CharField(blank=True, choices=[('change', 'change'), ('state', 'state')], help_text='Type of indicator', max_length=20)),
             ],
         ),
         migrations.CreateModel(
@@ -71,6 +71,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='product',
             name='wetlands',
-            field=models.ManyToManyField(blank=True, related_name='swos_product_wetlands', to='swos.Wetland', verbose_name=b'Wetlands'),
+            field=models.ManyToManyField(blank=True, related_name='swos_product_wetlands', to='swos.Wetland', verbose_name='Wetlands'),
         ),
     ]
