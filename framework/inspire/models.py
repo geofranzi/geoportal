88c8a1936a65ff80f3d3544b68130ede2bcbc277
Franzi <franzi82@gmx.de>
2020-07-10 12:57:22 +0200

    rename module to just inspire

--- framework/inspire/models.py
+++ framework/inspire/models.py
@@ -6,7 +6,7 @@ from rest_framework import serializers
 from django.contrib.gis.db import models
 
 
-from layers.models import Layer, ISOcodelist, KeywordInline, Contact
+from layers.models import Layer, ISOcodelist, KeywordInline, Contact, INSPIREthemes
 from content.models import Country
 from geospatial.models import Region
 
@@ -14,10 +14,14 @@ import json
 
 class inpire_opendataLayer(Layer):
     internal_contact =models.ForeignKey(Contact, related_name="internal_contact", verbose_name="Internal Contact", blank=True, null=True)
-    responsible_city_department = models.ForeignKey(Contact, related_name=" ", limit_choices_to= Q(organisation_startswith="Stadt Hameln - Fachabteilung"))
+    responsible_city_department = models.ForeignKey(Contact, related_name="city_department", limit_choices_to= Q(organisation_startswith="Stadt Hameln - Fachabteilung"))
     type = models.TextField(max_length=20, choices=[('intern', 'intern'), ('OpenData', 'OpenData'), ('INSPIRE', 'INSPIRE')], default="intern")
     status = models.TextField(max_length=20, )
-    #transformation
+    inspire_theme = models.ManyToManyField(INSPIREthemes, blank=True, related_name="inspire_theme")
+
+    def __unicode__(self):
+        return u"%s" % (self.identifier)
+
 
 
 
