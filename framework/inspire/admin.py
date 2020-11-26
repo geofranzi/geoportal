b416ff9cab905b42de2680b1780697e57677abf0
Franzi <franzi82@gmx.de>
2020-11-26 22:59:34 +0100

    Adjust INSPIRE model #6

--- framework/inspire/admin.py
+++ framework/inspire/admin.py
@@ -1,11 +1,64 @@
 from django.contrib.gis import admin
-from suit.sortables import SortableModelAdmin
-
+from django.contrib import messages
+from django.utils.translation import ngettext
 from suit.sortables import SortableTabularInline
 
 from layers.admin import LayersAdmin
-from .models import InpireLayer, ProcessingInline, InspireMap
+from map.admin import MapAdmin
+from .models import InspireDataset, ProcessingInline, InspireMap, InspireTheme, SourceLayer
 from content.admin import make_downloadable, make_non_downloadable, make_publishable, make_unpublishable
+from inspire.csw import create_update_csw, delete_csw, create_record, create_csw_xml, create_csw_view_xml
+
+
+def create_csw(self, request, queryset):
+    count = len(queryset)
+    for item in queryset:
+        result = create_csw_xml(item, "inspire")
+        if result["error"]:
+            self.message_user(request, result["error_msg"], messages.ERROR)
+            count = count - 1
+
+    self.message_user(request, ngettext(
+        '%d file was successfully created',
+        '%d files were successfully created.',
+        count,
+    ) % count, messages.SUCCESS)
+
+
+create_csw.short_description = "Create INSPIRE Dataset XML"
+
+def create_csw_source(self, request, queryset):
+    count = len(queryset)
+    for item in queryset:
+        result = create_csw_xml(item, "source")
+        if result["error"]:
+            self.message_user(request, result["error_msg"], messages.ERROR)
+            count = count - 1
+
+    self.message_user(request, ngettext(
+        '%d file was successfully created',
+        '%d files were successfully created.',
+        count,
+    ) % count, messages.SUCCESS)
+
+
+create_csw_source.short_description = "Create Source Dataset XML"
+
+def update_csw(self, request, queryset):
+    for item in queryset:
+        create_update_csw(item, True)
+
+
+update_csw.short_description = "Create and Update CSW"
+
+
+def create_view_csw(modeladmin, request, queryset):
+    for item in queryset:
+        create_csw_view_xml(item, True)
+
+
+create_view_csw.short_description = "Create View/Service XML"
+
 
 class ProcessingInlineTab(SortableTabularInline):
     model = ProcessingInline
@@ -13,45 +66,81 @@ class ProcessingInlineTab(SortableTabularInline):
     verbose_name_plural = "Processing"
     suit_classes = 'suit-tab suit-tab-processing'
 
-class InspireLayerAdmin(LayersAdmin):
+
+class SourceLayerAdmin(LayersAdmin):
     fieldsets = LayersAdmin.fieldsets + (
-    (None, {
-        'classes': ('suit-tab', 'suit-tab-internal',),
-        'fields': ('internal_contact', 'internal_responsible_city_department', 'internal_legal_basis', 'internal_access_constraint')
-    }),
-    (None, {
-        'classes': ('suit-tab', 'suit-tab-inspire',),
-        'fields': ('inspireidentified', 'inspire_title', 'inspire_abstract', 'inspire_epsg', 'inspire_theme', 'processing_new_dataset')
-    }),
+        (None, {
+            'classes': ('suit-tab', 'suit-tab-internal',),
+            'fields': ('internal_contact', 'internal_responsible_city_department', 'internal_legal_basis',
+                       'internal_access_constraint', 'internal_comment')
+        }),
+        (None, {
+            'classes': ('suit-tab', 'suit-tab-inspire',),
+            'fields': ('inspireidentified', 'opendata', 'inspire_theme')
+        }),
     )
-    inlines = LayersAdmin.inlines + (ProcessingInlineTab, )
-    list_display=('title','publishable', 'downloadable', 'internal_contact')
-    suit_form_tabs = LayersAdmin.suit_form_tabs + (('internal','Internal'),('inspire','INSPIRE'),('processing', 'Processing'))
-    search_fields=('title','abstract','inspire_theme__name')
-    ordering =['title']
-    list_filter=('publishable', 'downloadable')
+    inlines = LayersAdmin.inlines
+    list_display = ('title', 'publishable', 'downloadable', 'internal_contact')
+    suit_form_tabs = LayersAdmin.suit_form_tabs + (
+        ('internal', 'Internal'), ('inspire', 'INSPIRE'))
+    search_fields = ('title', 'abstract', 'inspire_theme__name')
+    ordering = ['title']
+    list_filter = ('publishable', 'downloadable')
     suit_list_filter_horizontal = ('publishable', 'downloadable')
 
-    actions=[make_publishable,make_unpublishable, make_downloadable, make_non_downloadable]
+    actions = [make_publishable, make_unpublishable, make_downloadable, make_non_downloadable, create_csw_source]
 
-class InspireMapAdmin(admin.ModelAdmin):
-    fieldsets =  (
+
+class InspireLayerAdmin(LayersAdmin):
+    fieldsets = LayersAdmin.fieldsets + (
+
         (None, {
-            'classes': ('suit-tab', 'suit-tab-contact',),
-            'fields': ('coordination_contact', 'data_contact', 'service_contact', 'metadata_contact')
-        }),
-        (None, {
-            'classes': ('suit-tab', 'suit-tab-wms',),
-            'fields': ('ows_contact', 'ows_title_de', 'ows_title_en', 'ows_abstract_de', 'ows_abstract_en', 'ows_rootlayer_title_de')
+            'classes': ('suit-tab', 'suit-tab-inspire',),
+            'fields': ('inspireidentified', 'opendata', 'inspire_theme')
         }),
     )
-   # inlines = (ProcessingInline, )
-    list_display=('ows_title_de','ows_title_en')
-    suit_form_tabs = (('contact','contact'),('wms','OWS/WMS/WFS'))
-  #  search_fields=('title','abstract','inspire_theme__name')
-  #  ordering =['title']
- #  list_filter=('publishable', 'downloadable')
-  #  suit_list_filter_horizontal = ('publishable', 'downloadable')
+    inlines = LayersAdmin.inlines + (ProcessingInlineTab,)
+    list_display = ('title', 'publishable', 'downloadable')
+    suit_form_tabs = LayersAdmin.suit_form_tabs + (
+        ('inspire', 'INSPIRE'),)
+    search_fields = ('title', 'abstract', 'inspire_theme__name')
+    ordering = ['title']
+    list_filter = ('publishable', 'downloadable')
+    suit_list_filter_horizontal = ('publishable', 'downloadable')
 
-admin.site.register(InpireLayer, InspireLayerAdmin)
+    actions = [make_publishable, make_unpublishable, make_downloadable, make_non_downloadable, update_csw, create_csw]
+
+
+class InspireMapAdmin(MapAdmin):
+    fieldsets = MapAdmin.fieldsets + (
+        (None, {
+            'classes': ('suit-tab', 'suit-tab-inspire',),
+            'fields': ('inspire_theme',)
+        }),
+
+    )
+    inlines = MapAdmin.inlines
+    #  list_display=('title','publishable', 'downloadable', 'internal_contact')
+    suit_form_tabs = MapAdmin.suit_form_tabs + (('inspire', 'Inspire theme'),)
+    search_fields = ( 'abstract', 'inspire_theme__name')
+
+    actions = [create_view_csw]
+
+
+# ordering =['title']
+# list_filter=('publishable', 'downloadable')
+# suit_list_filter_horizontal = ('publishable', 'downloadable')
+
+# actions=[make_publishable,make_unpublishable, make_downloadable, make_non_downloadable, update_csw, create_csw]
+
+class InspireThemeAdmin(admin.ModelAdmin):
+    list_display = ('uri', 'name_en', 'name_de', 'definition_en', 'definition_de', "topicCategory")
+
+    def has_add_permission(self, request):
+        return False
+
+
 admin.site.register(InspireMap, InspireMapAdmin)
+admin.site.register(SourceLayer, SourceLayerAdmin)
+admin.site.register(InspireDataset, InspireLayerAdmin)
+admin.site.register(InspireTheme, InspireThemeAdmin)
--- framework/inspire/models.py
+++ framework/inspire/models.py
@@ -1,31 +1,60 @@
-from django.db import models
 from django.contrib.auth.models import User, Group
 from django.db.models import Q
-from django.http import Http404, HttpResponse
 from rest_framework import serializers
 from django.contrib.gis.db import models
 
-
-from layers.models import Layer, ISOcodelist, KeywordInline, Contact, INSPIREthemes, ISOcodelistSerializer
+from layers.models import Layer, ISOcodelist, KeywordInline, Contact, MetadataSerializer
+from map.models import Map, MapLayerInline, MapSerializer
 from content.models import Country
 from geospatial.models import Region
 
-import json
 
-class InpireLayer(Layer):
+class InspireTheme(models.Model):
+    uri = models.CharField(max_length=400, verbose_name="URI")
+    name_en = models.CharField(max_length=200, verbose_name="Name (en)")
+    name_de = models.CharField(max_length=200, verbose_name="Name (de)")
+    definition_en = models.CharField(max_length=1000, verbose_name="Definition (en)")
+    definition_de = models.CharField(max_length=1000, verbose_name="Definition (de)")
+    topicCategory = models.ForeignKey(ISOcodelist, limit_choices_to={'code_list': "MD_TopicCategoryCode"}, related_name="topicCategory", blank=True, null=True)
+
+    def __str__(self):
+        return u"%s" % self.name_en
+
+
+class InspireThemesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InspireTheme
+        fields = ('uri', 'name_en', 'name_de', 'definition_en', 'definition_de', 'topicCategory')
+
+class SourceLayer(Layer):
     internal_contact = models.ForeignKey(Contact, related_name="internal_contact", verbose_name="Internal Contact", blank=True, null=True)
-    internal_responsible_city_department = models.ForeignKey(Contact, related_name="city_department", limit_choices_to= Q(organisation__startswith="Stadt Hameln"))
+    internal_responsible_city_department = models.ForeignKey(Contact, related_name="city_department",
+                                                         limit_choices_to=Q(organisation__startswith="Stadt Hameln"))
     internal_legal_basis = models.TextField(max_length=1000, blank=True, null=True)
     internal_access_constraint = models.TextField(max_length=300, blank=True, null=True)
+    internal_comment = models.TextField(max_length=1000, blank=True, null=True)
+
+    internal_pg_table_name = models.TextField(max_length=1000, blank=True, null=True)
+
+    opendata = models.BooleanField(default=False)
     inspireidentified = models.BooleanField(default=False)
-    inspire_title = models.TextField(max_length=500, blank=True, null=True)
-    inspire_abstract =  models.TextField(max_length=1000, blank=True, null=True)
-    inspire_epsg = models.IntegerField("EPSG code from the transformed dataset", blank=True, null=True, help_text="Just the projection code/number")
-    inspire_theme = models.ManyToManyField(INSPIREthemes, blank=True, related_name="inspire_theme")
+
+    inspire_theme = models.ManyToManyField(InspireTheme, blank=True, related_name="source_inspire_theme")
+
+class InspireDataset(Layer):
+
+    inspire_theme = models.ManyToManyField(InspireTheme, blank=True, related_name="inspire_theme")
+
+    opendata = models.BooleanField(default=False)
+    inspireidentified = models.BooleanField(default=False)
+
+    inspire_published = models.BooleanField(default=False)
+    inspire_first_publication_date = models.DateTimeField(verbose_name="First publication date",  blank=True, null=True)
+    inspire_last_publication_date = models.DateTimeField(verbose_name="Last publication date",  blank=True, null=True)
     processing_new_dataset = models.BooleanField("The content of the source dataset changes after processing", default=False)
 
     def __str__(self):
-        return u"%s" % (self.identifier)
+        return u"%s" % self.title
 
 
 class ProcessingInline(models.Model):
@@ -33,12 +62,13 @@ class ProcessingInline(models.Model):
     active = models.BooleanField(default=False)
     date = models.DateTimeField(verbose_name="execution date")
     description = models.CharField(max_length=2000)
-    input_table =  models.CharField(max_length=300)
-    output_table =  models.CharField(max_length=300)
-    layer = models.ForeignKey(InpireLayer, related_name='layer_processing')
+    input_table = models.CharField(max_length=300)
+    output_table = models.CharField(max_length=300)
+    output_gml = models.FileField("Output GML", upload_to="GML", null=True, blank=True, help_text="Upload output GML")
+    layer = models.ForeignKey(InspireDataset, related_name='layer_processing')
 
     def __unicode__(self):
-        return self.layer.inspire_title + " " + str(self.active)
+        return self.layer.title + " " + str(self.active)
 
     def save(self, *args, **kwargs):
         if self.active:
@@ -55,76 +85,35 @@ class ProcessingInline(models.Model):
         ordering = ['order']
 
 
-class InspireMap(models.Model):
-    coordination_contact = models.ForeignKey(Contact, related_name="coordination_contact", verbose_name="Coordintion", blank=True, null=True)
-    data_contact = models.ForeignKey(Contact, related_name="data_contact", verbose_name="Data contact", blank=True, null=True)
-    service_contact = models.ForeignKey(Contact, related_name="service_contact", verbose_name="Service Contact", blank=True, null=True)
-    metadata_contact = models.ForeignKey(Contact, related_name="metadata_contact", verbose_name="Metadata Contact", blank=True, null=True)
-    ows_contact = models.ForeignKey(Contact, related_name="wms_contact", verbose_name="WMS Contact", blank=True, null=True)
-    ows_title_de = models.CharField(max_length=200, null=True, blank=True)
-    ows_title_en = models.CharField(max_length=200, null=True, blank=True)
-    ows_abstract_de = models.CharField(max_length=2000, null=True, blank=True)
-    ows_abstract_en = models.CharField(max_length=2000, null=True, blank=True)
-    ows_rootlayer_title_de =models.CharField(max_length=200, null=True, blank=True)
-    ows_rootlayer_abstract_en = models.CharField(max_length=2000, null=True, blank=True)
-    ows_srs = models.CharField(max_length=200, null=True, blank=True, default="EPSG:3857,EPSG:4258,EPSG:4326,EPSG:3034,EPSG:3035,EPSG:25832,EPSG:31467")
-    ows_languages = models.CharField(max_length=200, null=True, blank=True, default="ger", help_text="Add more languages separated by Comma e.g. ger, eng")
-    ows_enable_request =  models.CharField(max_length=200, null=True, blank=True, default="*", help_text="GetCapabilities, GetMap, GetFeatureInfo and GetLegendGraphic. A ”!” in front of a request will disable the request. “*” enables all requests.")
-    inspire_theme = models.ManyToManyField(INSPIREthemes, blank=True, related_name="inspire_theme_map")
+class InspireMap(Map):
+    inspire_theme = models.ManyToManyField(InspireTheme, blank=True, related_name="inspire_theme_map")
+
+    inspire_wms_published = models.BooleanField(default=False)
+    inspire_wms_first_publication_date = models.DateTimeField(verbose_name="First publication date", blank=True, null=True)
+    inspire_wms_last_publication_date = models.DateTimeField(verbose_name="Last publication date", blank=True, null=True)
+
+    inspire_wfs_published = models.BooleanField(default=False)
+    inspire_wfs_first_publication_date = models.DateTimeField(verbose_name="First publication date", blank=True, null=True)
+    inspire_wfs_last_publication_date = models.DateTimeField(verbose_name="Last publication date", blank=True, null=True)
+
+class InspireMetadataSerializer(MetadataSerializer):
+    inspire_theme = InspireThemesSerializer(many=True)
+
+    class Meta(MetadataSerializer.Meta):
+        model = InspireDataset
+        fields = MetadataSerializer.Meta.fields + ( 'inspire_theme', )
+
+class SourceMetadataSerializer(MetadataSerializer):
+    inspire_theme = InspireThemesSerializer(many=True)
+
+    class Meta(MetadataSerializer.Meta):
+        model = SourceLayer
+        fields = MetadataSerializer.Meta.fields + ( 'inspire_theme', )
 
 
-   # wms_rootlayer_keywordlist =
-   # keyword model here (warn if one is missing from linked layer)
+class InspireMapSerializer(MapSerializer):
+    inspire_theme = InspireThemesSerializer(many=True)
 
-class KeywordMapInline(models.Model):
-    order = models.PositiveIntegerField(default=0)
-    keyword = models.CharField(max_length=200)
-    thesaurus_name = models.CharField(max_length=300, blank=True, null=True)
-    thesaurus_date = models.DateField(blank=True, null=True, verbose_name="Thesaurus publication date")
-    thesaurus_date_type_code_code_value = models.ForeignKey(ISOcodelist,limit_choices_to={'code_list': "CI_DateTypeCode"},blank=True, null=True, related_name="map_thesaurus_date_type_code_code_value")
-    layer = models.ForeignKey(InspireMap, related_name='layer_keywords')
-
-    def __str__(self):
-        return self.keyword
-
-class KeywordInlineSerializer(serializers.ModelSerializer):
-    thesaurus_date_type_code_code_value = ISOcodelistSerializer(read_only=True)
-
-    class Meta:
-        model = KeywordInline
-        fields = ('keyword', 'thesaurus_name', 'thesaurus_date', 'thesaurus_date_type_code_code_value')
-
-class MapStyle(models.Model):
-    name = models.CharField(max_length=400)
-    style_def= models.CharField(max_length=2000, null=True, blank=True)
-
-    def __unicode__(self):
-        return self.name
-
-class InspireMapLayer(models.Model):
-    order = models.PositiveIntegerField(default=0)
-    ows_group_title_de = models.CharField(max_length=200, null=True, blank=True)
-    ows_group_title_en = models.CharField(max_length=200, null=True, blank=True)
-    ows_group_abstract_de = models.CharField(max_length=2000, null=True, blank=True)
-    ows_group_abstract_en = models.CharField(max_length=2000, null=True, blank=True)
-
-    ows_layer_title_de = models.CharField(max_length=200, null=True, blank=True)
-    ows_layer_title_en = models.CharField(max_length=200, null=True, blank=True)
-    ows_layer_abstract_de= models.CharField(max_length=2000, null=True, blank=True)
-    ows_layer_abstract_en= models.CharField(max_length=2000, null=True, blank=True)
-
-    wms_layer_visibility_from = models.IntegerField(verbose_name="Visibility from", blank=True, null=True, help_text="Lower value")
-    wms_layer_visibility_to = models.IntegerField(verbose_name="Visibility to", blank=True, null=True, help_text="Higher value")
-
-    wms_layer_style = models.ForeignKey(MapStyle, related_name="layer_style")
-    ows_enable_request =  models.CharField(max_length=200, null=True, blank=True, default="*", help_text="GetCapabilities, GetMap, GetFeatureInfo and GetLegendGraphic. A ”!” in front of a request will disable the request. “*” enables all requests.")
-
-    def __unicode__(self):
-        return self.ows_group_title_de + " " + self.ows_group_title_en
-
-    class Meta:
-        ordering = ['order']
-
-
-
-
+    class Meta(MapSerializer.Meta):
+        model = InspireMap
+        fields = MapSerializer.Meta.fields + ('inspire_theme', )
